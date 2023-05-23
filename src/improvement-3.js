d39e3de621d40a60af95ccae79a673df3ace1ab1
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-05-23 15:54:38 +0200

    refactor: remove unnecessary comment

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -9,6 +9,5 @@ export default function findLargestNumber(numbers) {
   //check if the numbers parameter is an array or empty. If so return null
   if (!Array.isArray(numbers) || numbers.length === 0) return null;
 
-  // largest number in the array
   return Math.max(...numbers);
 }
